49285bb52e01b442c4166d6c5e7d5172fc6f6df7
Tyler Kinnaird <tmkinnaird87@gmail.com>
2021-01-29 13:40:51 -0700

    changed

--- shopping_app/shop-database/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :carts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products
-end
---
+++ src/Components/CartPage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+// import {Link} from 'react-router-dom';
+
+
+function CartPage () {
+    const [cartProducts, setcartProducts] = useState([]);
+
+    const fetchcartProducts = async () => {
+        try {
+            const res = await fetch('http://localhost:3000/products');
+            const json = await res.json();
+            setcartProducts(json);
+        }catch(error) {
+        console.log(error); 
+        }
+    };
+
+    // const deleteProduct = async (id) => {
+    //     try {
+    //       const response = await fetch(`http://localhost:3000/cartProducts/${id}`, {
+    //         method: "DELETE",
+    //         headers: {
+    //           "Content-type": "application/json",
+    //         },
+    //       });
+    //       const data = await response.json();
+    //       const filteredProducts = cartProducts.filter((product) => product.id !== data.product.id);
+    //       setProducts(filteredProducts);
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
+
+    useEffect(() => {
+        fetchcartProducts();
+    }, [cartProducts]);
+
+    return (
+        <>
+            
+            <div className="cart">
+                {cartProducts.map((product) => {
+                    return (
+                        <div
+                        key={product.id}
+                        >
+                        <p>{product.name} {product.description} ${product.price}</p>
+                        {/* <button onClick={(event) => {removeProduct(product.id);}}> Remove From Cart </button> */}
+                        </div>
+                         
+                    );
+                })}
+            </div>
+            
+        </>
+    );
+}
+
+export default CartPage;
